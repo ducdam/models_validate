3e1629bc492f51e70ab2e108a805469a6f32c1ec
ducdam <quangduc035@gmail.com>
2024-07-08 11:35:29 +0700

    xoa file khong can thiet

--- lib/models/re_password.dart
+++ lib/models/re_password.dart
@@ -2,11 +2,11 @@ import 'package:formz/formz.dart';
 
 enum RePasswordValidationError { invalid, mismatch }
 
-class ReNewPassword extends FormzInput<String, RePasswordValidationError> {
+class RePassword extends FormzInput<String, RePasswordValidationError> {
   final String originalPassword;
 
-  const ReNewPassword.pure({this.originalPassword = ''}) : super.pure('');
-  const ReNewPassword.dirty({required this.originalPassword, String value = ''}) : super.dirty(value);
+  const RePassword.pure({this.originalPassword = ''}) : super.pure('');
+  const RePassword.dirty({required this.originalPassword, String value = ''}) : super.dirty(value);
 
   @override
   RePasswordValidationError? validator(String? value) {
